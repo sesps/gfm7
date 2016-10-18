9072de9b367dfbc0069e9d8099e5d7ef1ff56708
lighthall <lighthall@2b1ce1e5-f9e2-4de2-b566-7fb11778057d>
2016-10-18 15:48:10 +0000

    Added beeping at end of run, increased maximum number of iterations from 99 to 999, corrected iteration number printing, added comment guides for formatting, comments

--- auto_gfm.cpp
+++ auto_gfm.cpp
@@ -2,15 +2,16 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-#include <cstdlib>
-#include <iomanip>      // std::setfill, std::setw
+#include <cstdlib>  //for system
+#include <iomanip>  //file formatting; std::setfill, std::setw
+#include <unistd.h> //for sleep
 
 using namespace std;
 
 int main()
 {
-  float energy[99];
-  float counts[99];
+  float energy[999];
+  float counts[999];
   char const* fname="temp.lst"; //name of file with list energies and counts
   
   ifstream infile(fname);
@@ -27,11 +28,19 @@ int main()
   for(int j=0;j<i;j++)
     {
       std::ofstream outfile("head.dat"); //name of default output file
-      outfile << "Iteration " <<j<< " of "<<i<<": energy="<<energy[j]<<" MeV, counts="<<counts[j]<< endl; //set title
-      outfile << left << setw(5) << counts[j] +2 << "500  0    3    0    2    0    12   015  NR,NP,NSKIP,JFOCAL,JMTRX,JNR,NPLT,NRXS,LPAX" << endl;
-      outfile << left<< setw(10) << energy[j] << "00.  90.0 .0   12.  32.  26.  15        0.0       ENERGY,DEN,TTGT,XNEN,ZTGT,PMASS,ATGT,Q0,DMASS" << endl;
+      outfile << "Iteration " <<j+1<< " of "<<i<<": energy="<<energy[j]<<" MeV, counts="<<counts[j]<< endl; //set title
+      //                                        NR--|NP--|NSKI|JFOC|JMTR|JNR-|NPLT|NRXS|LPAX|
+      outfile << left << setw(5) << counts[j] +2 << "500  0    3    0    2    0    12   015  NR,NP,NSKIP,JFOCAL,JMTRX,JNR,NPLT,NRXS,LPAX" << endl; //make no adjustments
+      //                                        ENERGY---|DEN-|TTGT|XNEN|ZTGT|PMAS|ATGT|Q0-------|DMASS----|
+      outfile << left<< setw(10) << energy[j] <<         "00.  90.0 .0   12.  38.9626.  17        0.0       ENERGY,DEN,TTGT,XNEN,ZTGT,PMASS,ATGT,Q0,DMASS" << endl; //set vales
       system("cat head.dat base.dat > input.dat");
       system("./gfm7.exe"); 
       system("cat input.mca >> output.mca");
     }
+  printf("End of runs!\a\n");
+  sleep(1);
+  printf(" beep!\a\n");
+  sleep(1);
+  printf(" beep!\a\n");
+  return 0;
 }
