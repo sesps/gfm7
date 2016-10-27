9a8ecb9ffc2d41d601d394465eda68546553877a
lighthall <lighthall@2b1ce1e5-f9e2-4de2-b566-7fb11778057d>
2016-10-27 21:57:36 +0000

    changed input file name from temp.lst to rand.lst; added fourth line of input file (removed from base file)

--- auto_gfm.cpp
+++ auto_gfm.cpp
@@ -12,7 +12,7 @@ int main()
 {
   float energy[999];
   float counts[999];
-  char const* fname="temp.lst"; //name of file with list energies and counts
+  char const* fname="rand.lst"; //name of file with list energies and counts
   
   ifstream infile(fname);
   printf("Reading list file \"%s\"\n",fname);
@@ -32,7 +32,9 @@ int main()
       //                                        NR--|NP--|NSKI|JFOC|JMTR|JNR-|NPLT|NRXS|LPAX|
       outfile << left << setw(5) << counts[j] +2 << "500  0    3    0    2    0    12   015  NR,NP,NSKIP,JFOCAL,JMTRX,JNR,NPLT,NRXS,LPAX" << endl; //make no adjustments
       //                                        ENERGY---|DEN-|TTGT|XNEN|ZTGT|PMAS|ATGT|Q0-------|DMASS----|
-      outfile << left<< setw(10) << energy[j] <<         "00.  90.0 .0   12.  38.9626.  17        0.0       ENERGY,DEN,TTGT,XNEN,ZTGT,PMASS,ATGT,Q0,DMASS" << endl; //set vales
+      outfile << left<< setw(10) << energy[j] <<         "00.  90.0 .0   12.  38.9626.  17.       0.0       ENERGY,DEN,TTGT,XNEN,ZTGT,PMASS,ATGT,Q0,DMASS" << endl; //set vales; q0 must be zero for vacuum mode
+      //          GAS-|QOPT|AGAS|TFOI|ZGAS|FOIL|ZION-----|PRESS----|GASOPT---|ACAPT----|TFWOPT---|
+      outfile << "1.   1.   4.   .001 2.   My   17.       4.000     -0.074    0.153     -1        GAS,QOPT,AGAS,TFOIL,ZGAS,FOIL,ZION,PRESS,GASOPT,ACAPT,TFWOPT" << endl;
       system("cat head.dat base.dat > input.dat");
       system("./gfm7.exe"); 
       system("cat input.mca >> output.mca");
